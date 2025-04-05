6592621208d45fc9a797a24e83c6ca4638ada071
Stefan474 <Fanste474@gmail.com>
2025-04-05 19:57:35 +0200

    added styling and toggle for simple view

--- src/components/LevelTable.vue
+++ src/components/LevelTable.vue
@@ -1,25 +1,37 @@
 <script setup lang="ts">
 import StandardTable from './leveling-tables/StandardTable.vue'
+import { ref } from 'vue'
+const isDescriptionVisible = ref(false)
 </script>
 
 <template>
-  <div class="tabs tabs-border h-fit">
-    <input type="radio" name="my_tabs_2" class="tab" aria-label="Standard" />
-    <div class="tab-content border-base-300 bg-base-100"><StandardTable /></div>
+  <div class="bg-base-300 p-2 rounded-md">
+    <div class="w-full flex justify-start mt-2">
+      <div class="flex justify-center gap-2 align-middle">
+        <input type="checkbox" class="toggle" v-model="isDescriptionVisible" />
+        <p class="opacity-60 text-xs">hide details</p>
+      </div>
+    </div>
+    <div class="tabs tabs-border h-fit">
+      <input type="radio" name="my_tabs_2" class="tab" aria-label="Standard" checked="checked" />
+      <div class="tab-content tab-content-custom">
+        <StandardTable :showDescription="isDescriptionVisible" />
+      </div>
 
-    <input type="radio" name="my_tabs_2" class="tab" aria-label="Fast 8" checked="checked" />
-    <div class="tab-content border-base-300 bg-base-100"></div>
+      <input type="radio" name="my_tabs_2" class="tab" aria-label="Fast 8" />
+      <div class="tab-content tab-content-custom"></div>
 
-    <input type="radio" name="my_tabs_2" class="tab" aria-label="Fast 9" />
-    <div class="tab-content border-base-300 bg-base-100"></div>
+      <input type="radio" name="my_tabs_2" class="tab" aria-label="Fast 9" />
+      <div class="tab-content tab-content-custom"></div>
 
-    <input type="radio" name="my_tabs_2" class="tab" aria-label="1-cost reroll" />
-    <div class="tab-content border-base-300 bg-base-100"></div>
+      <input type="radio" name="my_tabs_2" class="tab" aria-label="1-cost reroll" />
+      <div class="tab-content tab-content-custom"></div>
 
-    <input type="radio" name="my_tabs_2" class="tab" aria-label="2-cost reroll" checked="checked" />
-    <div class="tab-content border-base-300 bg-base-100"></div>
+      <input type="radio" name="my_tabs_2" class="tab" aria-label="2-cost reroll" />
+      <div class="tab-content tab-content-custom"></div>
 
-    <input type="radio" name="my_tabs_2" class="tab" aria-label="3-cost reroll" />
-    <div class="tab-content border-base-300 bg-base-100"></div>
+      <input type="radio" name="my_tabs_2" class="tab" aria-label="3-cost reroll" />
+      <div class="tab-content tab-content-custom"></div>
+    </div>
   </div>
 </template>
--- src/components/leveling-tables/StandardTable.vue
+++ src/components/leveling-tables/StandardTable.vue
@@ -1,6 +1,15 @@
+<script setup lang="ts">
+defineProps({
+  showDescription: {
+    type: Boolean,
+    default: false,
+  },
+})
+</script>
+
 <template>
   <ul class="list bg-base-100 rounded shadow-md">
-    <li class="p-4 pb-2 text-xs opacity-90 tracking-wide">
+    <li class="p-2 pb-2 text-xs opacity-90 tracking-wide">
       Good in most situations. Go for this if you don't know what to play. Can pivot into
       <b class="text-accent opacity-100">2 and 3 cost reroll</b> and
       <b class="text-accent opacity-100">fast 8/9</b>. Good if you need multiple carries.
@@ -18,7 +27,7 @@
           </p>
         </div>
         <div class="text-xs uppercase font-semibold">Do not roll</div>
-        <p class="list-col-wrap text-xs mt-0.5 opacity-70">
+        <p class="list-col-wrap text-xs mt-0.5 opacity-70" :class="{ hidden: showDescription }">
           Buy XP on 2-1 unless terrible board or lose-streaking
         </p>
       </div>
@@ -36,7 +45,7 @@
           </p>
         </div>
         <div class="text-xs uppercase font-semibold">DO NOT ROLL</div>
-        <p class="list-col-wrap text-xs mt-0.5 opacity-70">
+        <p class="list-col-wrap text-xs mt-0.5 opacity-70" :class="{ hidden: showDescription }">
           Avoid leveling on 2-3 so you don't screw up your interest for 2-3 and 2-4 (carousel).
         </p>
       </div>
@@ -53,9 +62,47 @@
           </p>
         </div>
         <div class="text-xs uppercase font-semibold">stay on 20-30g+ after leveling up</div>
-        <p class="list-col-wrap text-xs mt-0.5 opacity-70">
+        <p class="list-col-wrap text-xs mt-0.5 opacity-70" :class="{ hidden: showDescription }">
           Do not roll on 6 unless you have a lot of pairs or are low on health and have some pairs.
           Don't spend more than 10-30g on rerolling unless you are low hp.
+        </p>
+      </div>
+    </li>
+
+    <li class="list-row">
+      <div class="text-4xl font-thin opacity-30 tabular-nums">7 </div>
+
+      <div class="list-col-grow">
+        <div>
+          <p>
+            <span class="text-warning">3-5</span>, <span class="text-accent"><b>4-1</b></span
+            >, <span class="text-error">4-2</span>
+          </p>
+        </div>
+        <div class="text-xs uppercase font-semibold">30g+ after leveling up</div>
+        <p class="list-col-wrap text-xs mt-0.5 opacity-70" :class="{ hidden: showDescription }">
+          You can level earlier in sets with strong economy (like set 14). Roll a bit if you need to
+          stabilize your board and need specific 3-stars. If you are healthy roll maybe 10g max, if
+          in risk of losing try to stabilize.
+        </p>
+      </div>
+    </li>
+
+    <li class="list-row">
+      <div class="text-4xl font-thin opacity-30 tabular-nums">8 </div>
+
+      <div class="list-col-grow">
+        <div>
+          <p>
+            <span class="text-warning">3-5</span>, <span class="text-accent"><b>4-1</b></span
+            >, <span class="text-error">4-2</span>
+          </p>
+        </div>
+        <div class="text-xs uppercase font-semibold">30g+ after leveling up</div>
+        <p class="list-col-wrap text-xs mt-0.5 opacity-70" :class="{ hidden: showDescription }">
+          You can level earlier in sets with strong economy (like set 14). Roll a bit if you need to
+          stabilize your board and need specific 3-stars. If you are healthy roll maybe 10g max, if
+          in risk of losing try to stabilize.
         </p>
       </div>
     </li>
