5fd16f7f96ce3c8e60aa1d0756579284be12b76f
Stefan474 <Fanste474@gmail.com>
2025-04-09 17:59:18 +0200

    added item assets for the db and set data display in the item view

--- src/App.vue
+++ src/App.vue
@@ -3,33 +3,8 @@
     <NavBar />
     <router-view />
   </div>
-
-  <div class="p-4">
-    <h2 class="text-xl font-bold mb-4">Test Supabase Data</h2>
-    <ul>
-      <li v-for="item in itemCombos" :key="item.id">
-        {{ item.component }}
-      </li>
-    </ul>
-  </div>
 </template>
 
 <script setup lang="ts">
 import NavBar from '@/components/NavBar.vue'
-import { ref, onMounted } from 'vue'
-import { supabase } from './supabase'
-
-interface ComponentItem {
-  id: number
-  component: string
-}
-
-const itemCombos = ref<ComponentItem[]>([])
-
-onMounted(async () => {
-  const { data, error } = await supabase.from('component_list').select('*')
-
-  if (error) console.error('Supabase error:', error)
-  else if (data) itemCombos.value = data as ComponentItem[]
-})
 </script>
--- src/views/ItemView.vue
+++ src/views/ItemView.vue
@@ -1,13 +1,40 @@
-<script lang="ts">
-import { defineComponent } from 'vue'
+<script setup lang="ts">
+import { ref, onMounted } from 'vue'
+import { supabase } from '../supabase'
 
-export default defineComponent({
-  name: 'ItemView',
+interface ComponentItem {
+  id: number
+  component: string
+  stat_type: string
+  asset_route: string
+}
+
+const itemCombos = ref<ComponentItem[]>([])
+
+onMounted(async () => {
+  const { data, error } = await supabase.from('component_list').select('*')
+  if (error) console.error('Supabase error:', error)
+  else if (data) itemCombos.value = data as ComponentItem[]
 })
 </script>
 
 <template>
   <div class="home">
     <h1>Item View</h1>
+    <div class="p-4">
+      <ul class="flex gap-2">
+        <li v-for="item in itemCombos" :key="item.id">
+          <div class="avatar">
+            <div class="w-12 rounded-xl">
+              <img
+                v-if="item.asset_route"
+                :src="'src/assets/item_images/' + item.asset_route + '.png'"
+                :alt="item.component + ' item icon'"
+              />
+            </div>
+          </div>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
