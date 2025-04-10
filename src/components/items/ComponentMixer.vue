20594b76ecc68f886e7db5ecd9a79dcba8d6b8f6
Stefan474 <Fanste474@gmail.com>
2025-04-10 02:53:20 +0200

    added a tooltip window for hovering components

--- src/components/items/ComponentMixer.vue
+++ src/components/items/ComponentMixer.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { ref, onMounted } from 'vue'
 import { supabase } from '../../supabase'
+import ItemPreviewWindow from './ItemPreviewWindow.vue'
 
 interface ComponentItem {
   id: number
@@ -9,26 +10,57 @@ interface ComponentItem {
   asset_route: string
 }
 
+const showWindow = ref(false)
+
 const itemCombos = ref<ComponentItem[]>([])
+
+const currentItem = ref<ComponentItem>()
+
+const loading = ref(true) // track loading state
 
 onMounted(async () => {
   const { data, error } = await supabase.from('component_list').select('*')
   if (error) console.error('Supabase error:', error)
   else if (data) itemCombos.value = data as ComponentItem[]
+  loading.value = false // done loading
 })
 </script>
 
 <template>
-  <div class="p-4">
-    <ul class="flex gap-2">
-      <li v-for="item in itemCombos" :key="item.id">
-        <div class="avatar">
-          <div class="w-12 rounded-xl">
+  <div class="p-4 h-full flex-col">
+    <div v-if="loading" class="flex justify-center py-8">
+      <span className="loading loading-bars loading-lg"></span>
+    </div>
+
+    <ul v-else class="flex gap-1 flex-wrap justify-center">
+      <li v-for="item in itemCombos" :key="item.id" class="relative">
+        <div
+          class="avatar"
+          @mouseenter="
+            () => {
+              currentItem = item
+              showWindow = true
+            }
+          "
+          @mouseleave="
+            () => {
+              showWindow = false
+            }
+          "
+        >
+          <div class="w-11 rounded-xl border-2 border-base-200">
             <img
               v-if="item.asset_route"
               :src="'/assets/item_images/' + item.asset_route + '.png'"
               :alt="item.component + ' item icon'"
             />
+          </div>
+
+          <div
+            v-if="showWindow && currentItem?.id === item.id"
+            class="absolute top-full left-1/2 -translate-x-1 z-10"
+          >
+            <ItemPreviewWindow :showWindow="showWindow" :item="item" />
           </div>
         </div>
       </li>
---
+++ src/components/items/ItemPreviewWindow.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+defineProps({
+  showWindow: {
+    type: Boolean,
+    default: false,
+  },
+  item: {
+    type: Object,
+    default: () => ({}),
+  },
+})
+</script>
+
+<template>
+  <div class="card bg-base-300 w-54 shadow-sm border-1 border-amber-200 mt-4">
+    <div class="card-body pb-0 border-b-base-100 border-b-1 p-2">
+      <div class="avatar">
+        <div class="w-11 rounded-xl border-2 border-base-200">
+          <img
+            v-if="item.asset_route"
+            :src="'/assets/item_images/' + item.asset_route + '.png'"
+            :alt="item.component + ' item icon'"
+          />
+        </div>
+        <h2 class="card-title ml-2">{{ item.component }}</h2>
+      </div>
+      <p>+{{ item.stat_value }} {{ item.stat_type }} <br /></p>
+      <div class="card-actions justify-end"></div>
+    </div>
+    <div class="card-body" v-if="item.description">
+      <p class="font-bold">
+        {{ item.description }}
+      </p>
+      <div class="card-actions justify-end"></div>
+    </div>
+  </div>
+</template>
