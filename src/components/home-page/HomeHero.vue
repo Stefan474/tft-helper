64a899b6adb9108403dbdaf9bb7f120320b9c5b7
Stefan474 <Fanste474@gmail.com>
2025-05-03 16:30:36 +0200

    add router link to hero button

--- src/components/home-page/HomeHero.vue
+++ src/components/home-page/HomeHero.vue
@@ -1,17 +1,22 @@
+<script setup lang="ts">
+import { useRouter } from 'vue-router'
+const router = useRouter()
+</script>
+
 <template>
-  <div class="hero min-h-screen flex w-full items-center justify-center">
-    <div class="hero-content w-1/2 h-full bg-base-300">
+  <div class="hero min-h-screen flex w-full items-center justify-center position-relative">
+    <div class="hero-content w-full lg:w-1/2 h-full bg-base-300">
       <div class="max-w-md">
         <h1 class="text-5xl font-bold">TFT Helper</h1>
         <p class="py-6">
           Simplify how you think about TFT and focus on playing instead of juggling different guide
           sites.
         </p>
-        <button class="btn btn-primary">Make a comp</button>
+        <button class="btn btn-primary" @click="router.push('/create')">Make a comp</button>
         <button class="btn btn-success mx-2">Import comps</button>
       </div>
     </div>
-    <div class="w-1/2 bg-base-300 h-lvh">
+    <div class="w-1/2 bg-base-300 h-lvh lg:block hidden position-absolute right-0">
       <img
         src="@/assets/transparent-hero.png"
         alt="teamfight tactics cover image"
