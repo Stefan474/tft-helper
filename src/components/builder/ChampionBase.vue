37fe35dc43d8e2127ed21d8e57369ec1ef5f3740
Stefan474 <Fanste474@gmail.com>
2025-05-04 13:25:19 +0200

    add search element

--- src/components/builder/ChampionBase.vue
+++ src/components/builder/ChampionBase.vue
@@ -28,7 +28,22 @@ onMounted(async () => {
 </script>
 
 <template>
-  <ul v-for="champion in championList" :key="champion.id">
-    <li>{{ champion.name }}</li>
-  </ul>
+  <label class="floating-label mt-7">
+    <span>Search</span>
+    <input type="text" placeholder="Search by name or trait" class="input input-md" />
+  </label>
+  <div class="flex w-full max-w-4xl gap-1 flex-wrap justify-start mt-5">
+    <ul v-for="champion in championList" :key="champion.id" class="flex gap-2 max-w-4xl">
+      <li>
+        <div class="w-15 rounded-xl border-2 border-base-200 h-15">
+          <img
+            v-if="champion.asset_path"
+            :src="'/assets/tft-champion/' + champion.asset_path + '.png'"
+            :alt="champion.name + ' champion icon'"
+            class="object-right object-cover h-full rounded-xl"
+          />
+        </div>
+      </li>
+    </ul>
+  </div>
 </template>
